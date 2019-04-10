715488f969cf60186e8079ae2c6f225e9a9e3646
Zeeshan Faisal <zeeskt74@hotmail.com>
2019-04-10 10:19:30 -0400

    Added Repo Interface

---
+++ GraduationTracker/GraduationTracker/Repositories/IRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraduationTracker.Repositories
+{
+    public interface IRepo<T>
+    {
+        T[] GetAll();
+        T GetById(int id);
+    }
+}
