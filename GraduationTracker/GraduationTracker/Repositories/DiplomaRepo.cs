db0a582beccdad3c60344df4606f7da241bebefc
Zeeshan Faisal <zeeskt74@hotmail.com>
2019-04-10 10:24:27 -0400

    Implemented repo stub

---
+++ GraduationTracker/GraduationTracker/Repositories/DiplomaRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraduationTracker.Repositories
+{
+    public class DiplomaRepo : IDiplomaRepo
+    {
+        public Diploma[] GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Diploma GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GraduationTracker/GraduationTracker/Repositories/StudentRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraduationTracker.Repositories
+{
+    public class StudentRepo : IStudentRepo
+    {
+        public Student[] GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
